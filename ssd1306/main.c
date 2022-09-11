afcf26b1534ebbf830d74c832bbaaf36badaa9ac
Lis0v <kokobolo@tutanota.com>
2022-09-11 22:44:14 +0200

    poprawione komendy

--- ssd1306/main.c
+++ ssd1306/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <util/delay.h>
 
 #include </usr/include/ssd1306/ssd1306.h>
 #include </usr/include/ssd1306/font.h>
@@ -10,23 +11,17 @@
 
 int main(void)
 {
-  uint8_t addr = SSD1306_ADDRESS;
+    uint8_t addr = SSD1306_ADDRESS;
+    SSD1306_Init (addr);
+    SSD1306_ClearScreen ();
 
-  SSD1306_Init (addr);
+    int x;
 
-  SSD1306_ClearScreen ();
-
-  SSD1306_DrawLine (0, MAX_X, 4, 4);
-
-  SSD1306_SetPosition (7, 1);
-  SSD1306_DrawString ("moj penis");
-
-  SSD1306_DrawLine (0, MAX_X, 18, 18);
-
-  SSD1306_SetPosition (40, 3);
-  SSD1306_DrawString ("nghhhh");
-
-  SSD1306_UpdateScreen (addr);
-
-  return 0;
+        for(int y=0;y<64;y++){
+            y = x + 1;
+            SSD1306_DrawPixel(x, y);
+            SSD1306_UpdateScreen (addr);
+            _delay_ms(1000);
+        }
+    return 0;
 }
